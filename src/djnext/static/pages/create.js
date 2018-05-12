1d445f68a39dfd5b662c68ba4db23b8dd35b5a0a
jpic <jamespic@gmail.com>
2018-05-12 03:01:31 +0200

    Missing tpls

---
+++ src/djnext/static/pages/create.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from 'material-ui/Button'
+
+const App = () => (
+  <Button variant="raised" color="primary">
+    Hello World from create2 yeah yeah
+  </Button>
+)
+
+export default App
---
+++ src/djnext_example/static/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from 'material-ui/Button'
+
+const App = () => (
+  <Button variant="raised" color="primary">
+    Hello World from index
+  </Button>
+)
+
+export default App
+
