660b666ce69a0b664b887daf97bb3a9391714011
jpic <jamespic@gmail.com>
2018-05-13 00:55:53 +0200

    Channels routing support

--- src/djnext_example/settings.py
+++ src/djnext_example/settings.py
@@ -6,23 +6,40 @@ globals().update(django12factor.factorise())
 SECRET_KEY = os.environ.get('SECRET_KEY', 'notsecret')
 
 INSTALLED_APPS = [
+    'djnext',
+
+    'djnext_example',  # load layout template
+    'djnext_example.artist',  # example app in this directory
+]
+
+try:
+    import channels
+except ImportError:
+    pass
+else:
+    # optional, for HTTTP/2
+    INSTALLED_APPS.append('channels')
+
+try:
+    import crudlfap
+except ImportError:
+    pass
+else:
+    # optional, for dev command
+    INSTALLED_APPS.append('crudlfap')
+
+INSTALLED_APPS += [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'djnext_example',  # load layout template
-    'djnext_example.artist',  # example app in this directory
-
-    'djnext',  # for isomorphic code love
-
-    'crudlfap',  # for dev command
 ]
 
 ROOT_URLCONF = 'djnext_example.urls'
 WSGI_APPLICATION = 'djnext_example.wsgi.application'
+ASGI_APPLICATION = 'djnext_example.routing.application'
 
 TEMPLATES = [
     {
